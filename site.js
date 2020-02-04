89c6990b32b6653e2625fcf163020cb126ff54cf
satyaelite <41486242+satyaelite@users.noreply.github.com>
2020-02-03 20:33:47 -0500

    sample pages

---
+++ site.js
@@ -0,0 +1,28 @@
+(function( site, undefined ) {
+
+    timeout = 400;
+
+ 
+    site.partypartyparty = function(e) {
+	var partyers = document.getElementsByClassName('party');
+	for(var i=0; i<partyers.length; i++) {
+            var r = Math.floor(Math.random()*255);
+            var g = Math.floor(Math.random()*255);
+            var b = Math.floor(Math.random()*255);
+            var e = partyers[i];
+	    e.style.setProperty('color', 'rgb('+ r +','+ g +',' + b + ')');
+
+	    if (Math.random()*100 > 99) {
+		e.innerText = 'PARTY! PARTY! PARTY!';
+	    }
+	    
+	    this.timeout = Math.max(this.timeout * 0.9, 50);
+	}
+
+        window.setTimeout(site.partypartyparty, this.timeout);
+    };
+
+}( window.site = window.site || {} ));
+
+site.partypartyparty();
+
